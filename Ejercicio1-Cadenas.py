f30c42e5e5938a32b0b4981f6dcc0e9793a5e055
Jhohan Cabrera <jhohancabrera670@gmail.com>
2024-03-25 18:15:32 -0500

    Ejercicios

---
+++ Ejercicio1-Cadenas.py
@@ -0,0 +1,10 @@
+var = "Te quiero solo como amigo"
+
+#print(len(var)) #Conocer el tamaño de la cadena
+print(var[:2]) # Imprimir los dos primeros caracteres
+print(var[22:]) #Imprimir los ultimos tres caracteres
+print(var[::2]) #Imprimir la cadena cada dos caracteres
+print(var[::-1]) #Imprimir la cadena en sentido inverso
+print(var+var[::-1]) #Imprimir la cadena en un sentido y sentido inverso
+
+
---
+++ Ejercicio2.py
@@ -0,0 +1,9 @@
+p_payasos = 112
+p_muneca = 75
+
+cant_payasos = 23
+cant_munecas = 54
+
+peso_total = (p_payasos * cant_payasos) + (p_muneca * cant_munecas)
+
+print(f"El peso total de los {cant_payasos} payasos y las {cant_munecas} munecas es: {peso_total} gramos") 
