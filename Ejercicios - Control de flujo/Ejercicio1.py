ec0f003f8cb917fd34f6f9f9ab3cb12d1c372b4b
Jhohan Cabrera <jhohancabrera670@gmail.com>
2024-03-26 21:48:57 -0500

    Ejercicios 19-03

---
+++ Ejercicios - Control de flujo/Ejercicio1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa otro numero: "))
+
+if num1 > num2:
+    print(f"{num1} es mayor que {num2}")
+elif num1 < num2:
+    print(f"{num1} es menor que {num2}")
+elif num1 == num2:
+    print(f"{num1} es igual a {num2}")
